ce31d2aa0bb7ebde110fc4ab13ab2fea4cc57316
ArthurFrade1 <arthurafonso730@gmail.com>
2023-01-29 12:44:08 -0300

    Commit

--- Contabilidade/Empregado.java
+++
@@ -1,5 +0,0 @@
-public abstract class Empregado implements Passivo{
-    protected String nome;
-    protected String sobrenome;
-    protected int numIdent;
-}
--- Contabilidade/Passivo.java
+++
@@ -1,6 +0,0 @@
-public interface Passivo {
-    double Salario=1212;
-    double Hora=45;
-
-    double getValorAPagar(int diaPagto, int MesPagto);
-}
